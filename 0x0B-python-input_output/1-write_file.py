79a9aeca7e03a77bac8d9ae32fcb052981d1eca9
gideonkiplagat <kiplagatgideon00@gmail.com>
2024-01-09 15:27:06 +0300

    writes a string to a text file (UTF8)

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Task2"""
+def number_of_lines(filename=""):
+    """ function that returns the number of lines of a text file """
+
+    with open(filename) as f:
+        count = len(f.readlines())
+    return count

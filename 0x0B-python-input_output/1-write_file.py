94a6c58c56caa344f123e5c5c8eeb8080d946de8
gideonkiplagat <kiplagatgideon00@gmail.com>
2024-01-09 15:29:26 +0300

    writes a string to a text file (UTF8)

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""Defines a file-writing function."""
 
-""" Task2"""
-def number_of_lines(filename=""):
-    """ function that returns the number of lines of a text file """
 
-    with open(filename) as f:
-        count = len(f.readlines())
-    return count
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)

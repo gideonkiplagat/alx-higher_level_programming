c4806b042185eb2e6d91199535345445f9da9392
gideonkiplagat <gideonkiplagat00@gmail.com>
2024-02-14 19:48:38 +0300

    unique ID

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Creates the table unique_id
+-- id INT with the default value 1 and must be unique
+CREATE TABLE IF NOT EXISTS `unique_id` (
+    `id`   INT DEFAULT 1 UNIQUE,
+    `name` VARCHAR(256)
+);

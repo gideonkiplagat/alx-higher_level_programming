ff34f29dfccf81d2670c41d212251204ecb0bcb2
gideonkiplagat <gideonkiplagat00@gmail.com>
2024-03-11 20:07:26 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 // prints a message depending of the number of arguments passed
-if (process.argv.length === 2) {
+if (process.argv.length === 0) {
     console.log('No argument');
-} else if(process.argv.length === 3) {
+} else if(process.argv.length === 1) {
     console.log('Argument found');
 } else {
     console.log('Arguments found')

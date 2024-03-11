2e8982885f1b8ee412d96fe2699d2540f5bd4834
gideonkiplagat <gideonkiplagat00@gmail.com>
2024-03-11 20:21:31 +0300

    An interger

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
+// prints two arguments passed to it, in the following format: “ is ”
 
-//prints My number: <first argument converted in integer>
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+if (isNaN(process.argv[2])) {
+    console.log('Not a number');
+  } else {
+    console.log('My number: ' + parseInt(process.argv[2]));
+  }

44e0fb70dfecfbd6a3fad77ef5e7da5920dd8b06
gideonkiplagat <gideonkiplagat00@gmail.com>
2024-03-11 19:51:51 +0300

    3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 //Write a script that prints 3 lines
-console.log('C is fun\Python is cool\JavaScript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');

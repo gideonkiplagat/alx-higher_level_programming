41a3e519e4b8ebdb28a95bd6bc99b51a0e8cd5fe
gideonkiplagat <gideonkiplagat00@gmail.com>
2024-03-17 09:17:11 +0300

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3], state = argv[4])
 
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name FROM cities\
